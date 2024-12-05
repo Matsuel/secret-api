bf2d1d4744abf3535bc52587a29ea0f5129ea374
Matsuel <matsuel0308@gmail.com>
2024-12-05 13:33:35 +0100

    Rename secrets to sekrets

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from users.router import users_router
 from spaces.router import spaces_router
-from secrets.router import secrets_router
+from sekrets.router import sekrets_router
 
 app = FastAPI()
 
 
 app.include_router(users_router)
 app.include_router(spaces_router)
+app.include_router(sekrets_router)
---
+++ src/sekrets/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+sekrets_router = APIRouter()
+
+@sekrets_router.post("/secret", tags=["secrets"])
+def create_secret():
+    pass
+
+@sekrets_router.put("/secret/:secret_id", tags=["secrets"])
+def update_secret_content(secret_id: int):
+    pass
+
+@sekrets_router.delete("/secret/:secret_id", tags=["secrets"])
+def delete_secret(secret_id: int):
+    pass
+
+@sekrets_router.post("/secret/:secret_id/like", tags=["secrets"])
+def like_secret(secret_id: int):
+    pass
