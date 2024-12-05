efbeb0ecc99b9bb39debe13aa772024b148b7f70
Matsuel <matsuel0308@gmail.com>
2024-12-05 15:16:42 +0100

    Edit user table

--- models/user.py
+++ models/user.py
@@ -7,6 +7,6 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String, nullable=False, unique=True)
-    api_key_id = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
     followsCount = Column(Integer, nullable=False, default=0)
     followersCount = Column(Integer, nullable=False, default=0)
