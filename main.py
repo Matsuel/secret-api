13cfbbb55ee8f61e5fae15b2d6d0efcbd4aab4a9
Matsuel <matsuel0308@gmail.com>
2025-01-10 09:32:30 +0100

    Main -> Add metadata to FastAPI

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from src.users.router import users_router
 from src.spaces.router import spaces_router
 from src.sekrets.router import sekrets_router
 from src.auth.router import auth_router
-from src.docs.router import docs_router
 from src.followers.router import followers_router
 from src.models.database import init_db
 
 init_db()
 
+description = """
+Documentation du projet Sekret API
+Ce projet est une API pour une application de type réseau social.
+Elle permet de créer des utilisateurs, de créer des espaces, de créer des sekrets et de suivre d'autres utilisateurs.
+"""
+
+
 app = FastAPI(
     title="Sekret API",
+    description=description,
+    version="1.0",
+    docs_url="/",
 )
 
 
@@ -19,4 +28,3 @@ app.include_router(followers_router)
 app.include_router(spaces_router)
 app.include_router(sekrets_router)
 app.include_router(auth_router)
-app.include_router(docs_router)
