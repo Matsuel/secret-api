97cd446687e6fb57a044196b1f15c10a95800d5b
Matsuel <matsuel0308@gmail.com>
2025-01-10 10:46:30 +0100

    Sekrets -> Split code

--- src/sekrets/router.py
+++ src/sekrets/router.py
@@ -1,120 +1,81 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from src.models.secret import Secret, CreateSecret, UpdateSecret
 from src.models.database import SessionLocal
 from src.models.database import SessionLocal
-from sqlalchemy import insert
+from src.sekrets.service import create_secret_db, get_all_secrets_from_db, get_secret_by_id, get_secret_by_space_id, update_secret_in_db, delete_secret_in_db, like_secret_in_db
 sekrets_router = APIRouter()
 
 ##############################
 # POST - Create a new secret
 
-@sekrets_router.post("/secret", tags=["secrets"])
+@sekrets_router.post("/secret", tags=["secrets"], status_code=201)
 def create_secret(secret: CreateSecret):
-    with SessionLocal() as session:
-        stmt = insert(Secret).values(
-            text = secret.text,
-            user_id = secret.user_id,
-            category_id = secret.category_id,
-            is_public = secret.is_public,
-            shared_space_id = secret.shared_space_id,
-            anonymous = secret.anonymous,
-            likesCount = secret.likesCount
-        )
-        session.execute(stmt)
-        session.commit()
-    return secret
+    result = create_secret_db(secret)
+    if not result:
+        raise HTTPException(status_code=400, detail="Error creating secret")
+    return {"message": "Secret created successfully"}
 
 ######################
 # Get all secrets
 
-@sekrets_router.get("/secrets", tags=["secrets"])
-def get_secrets_all():
-    with SessionLocal() as session:
-        stmt = session.query(Secret).all()
-    return stmt 
+@sekrets_router.get("/secrets", tags=["secrets"], response_model=list[Secret], status_code=200)
+def get_secrets_all(offset: int = 0, limit: int = 100):
+    result = get_all_secrets_from_db(offset, limit)
+    if not result:
+        raise HTTPException(status_code=404, detail="No secrets found")
+    return result
 
 #############################
 # Get a secret by secret_id
 
-@sekrets_router.get("/secret/{secret_id}", tags=["secrets"])
+@sekrets_router.get("/secret/{secret_id}", tags=["secrets"], response_model=Secret, status_code=200)
 def get_secret_id(secret_id: int):
-    with SessionLocal() as session:
-     stmt = session.query(Secret).filter(Secret.id == secret_id).first()
-    return stmt
+    result = get_secret_by_id(secret_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Secret not found")
+    return result
 
 ################################
 # Get secret by space_id
 
-@sekrets_router.get("/secret/space/{space_id}", tags=["secrets"])
+@sekrets_router.get("/secret/space/{space_id}", tags=["secrets"], response_model=Secret, status_code=200)
 def get_secrets(space_id: int):
-    with SessionLocal() as session:
-        stmt = session.query(Secret).filter(Secret.shared_space_id == space_id).first()
-    return stmt
+    result = get_secret_by_space_id(space_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Secret not found")
+    return result
 
 ################################
 # Update a secret by secret_id
 
-@sekrets_router.put("/secret/{secret_id}", tags=["secrets"])
+@sekrets_router.put("/secret/{secret_id}", tags=["secrets"], status_code=200)
 def update_secret_content(secret_id: int, secret: UpdateSecret):
-    with SessionLocal() as session:
-        stmt = session.query(Secret).filter(Secret.id == secret_id).first() 
-
-        if secret.text is not None: 
-            stmt.text = secret.text
-        
-        if secret.is_public is not None:
-            stmt.is_public = secret.is_public
-        if secret.anonymous is not None:
-            stmt.anonymous = secret.anonymous
-        
-        if secret.category_id is not None:
-            stmt.category_id = secret.category_id
-        
-        if secret.shared_space_id is not None:
-            stmt.shared_space_id = secret.shared_space_id
-
-        session.commit()
-
+    result = update_secret_in_db(secret_id, secret)
+    if not result:
+        raise HTTPException(status_code=404, detail="Secret not found")
     return {"message": "Secret updated successfully", "data": secret}
 
 #########################
 # Delete a secret by id
 
-@sekrets_router.delete("/secret/{secret_id}", tags=["secrets"])
+@sekrets_router.delete("/secret/{secret_id}", tags=["secrets"], status_code=200)
 def delete_secret(secret_id: int):
-    with SessionLocal() as session:
-        stmt = session.query(Secret).filter(Secret.id == secret_id).first()
-        session.delete(stmt)
-        session.commit()
-
+    result = delete_secret_in_db(secret_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Secret not found")
     return {"message": "Secret deleted successfully"}
-
-#! This route will delete all the secrets in the database
-@sekrets_router.delete("/secrets", tags=["secrets"])
-def delete_all_secret():
-    with SessionLocal() as session:
-        stmt = session.query(Secret).all()
-
-        for secret in stmt:
-            session.delete(secret)
-
-        session.commit()
-
-    return {"message": "All Secrets deleted successfully"}
 
 ################################
 # Like a secret by secret_id
 
-@sekrets_router.post("/secret/{secret_id}/like", tags=["secrets"])
+@sekrets_router.post("/secret/{secret_id}/like", tags=["secrets"], status_code=200)
 def like_secret(secret_id: int):
-    with SessionLocal() as session:
-        stmt = session.query(Secret).filter(Secret.id == secret_id).first()
-        stmt.likesCount += 1
-        session.commit()
-
+    result = like_secret_in_db(secret_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Secret not found")
     return {"message": "Secret liked successfully"}
 
-@sekrets_router.get("/secrets/popular", tags=["secrets"])
+@sekrets_router.get("/secrets/popular", tags=["secrets"], response_model=list[Secret], status_code=200)
 def get_popular_secrets(offset: int = 0, limit: int = 10):
     with SessionLocal() as session:
         stmt = session.query(Secret).order_by(Secret.likesCount.desc()).offset(offset).limit(limit).all()
