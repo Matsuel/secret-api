f8ad83713f367ce50e63a7e13d9604adb146e4dd
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2025-01-10 09:57:38 +0100

    Ajout des imports pour les modèles nécessaires à l'initialisation de la base de données -- ** pas d'utilité pour l'instant **

--- src/models/database.py
+++ src/models/database.py
@@ -20,4 +20,11 @@ Base = declarative_base()
 
 # Créer les tables au démarrage
 def init_db():
+    from .user import User
+    from .category import Category
+    from .follower import Follower
+    from .liked_secret import LikedSecret
+    from .secret import Secret
+    from .shared_space_user import SharedSpaceUser
+    from .shared_space import SharedSpace
     Base.metadata.create_all(bind=engine)
