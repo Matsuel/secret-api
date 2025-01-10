083c2534f05e9a26d3e98ec56808a594a1e93e6c
Matsuel <matsuel0308@gmail.com>
2025-01-10 16:25:58 +0100

    Get users secrets

--- src/users/router.py
+++ src/users/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from .service import get_users_list, get_user_by_id, create_user_in_db, delete_user_in_db, update_user_in_db
+from src.sekrets.service import get_secrets_by_user_id
 from fastapi import HTTPException
 from src.models.user import UserModelCreation, UserModel
 
@@ -10,9 +11,11 @@ def get_users(offset: int = 0, limit: int = 10):
     users = get_users_list(offset, limit)
     return users
 
-@users_router.get("/user/{user_id}", tags=["users"], response_model=UserModel)
+@users_router.get("/user/{user_id}", tags=["users"])
 def get_user_infos(user_id: int):
     user = get_user_by_id(user_id)
+    secrets = get_secrets_by_user_id(user_id)
+    user["secrets"] = secrets
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
