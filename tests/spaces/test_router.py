9260b799ce2fbf421cd9e0c964728d1805615a60
Lytzeer <lukasportier@gmail.com>
2025-01-18 03:16:04 +0100

    Ajout des tests des méthodes GET quand il n'y a pas de spaces

--- tests/spaces/test_router.py
+++ tests/spaces/test_router.py
@@ -12,3 +12,8 @@ class TestSpacesRouter(unittest.TestCase):
         response = self.client.get("/spaces")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), [])
+
+    def test_get_space_by_id_with_no_space(self):
+        response = self.client.get("/space/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), None)
