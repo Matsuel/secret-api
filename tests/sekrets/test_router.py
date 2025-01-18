bc8d23081a9cf3f313b298160ea39744ea24e079
Lytzeer <lukasportier@gmail.com>
2025-01-18 03:13:56 +0100

    Ajout des tests des méthodes GET quand il n'y a pas de secrets

--- tests/sekrets/test_router.py
+++ tests/sekrets/test_router.py
@@ -12,3 +12,18 @@ class TestSekretRouter(unittest.TestCase):
         response = self.client.get("/secrets")
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response.json(), {"detail": "No secrets found"})
+
+    def test_get_sekret_by_id_with_no_sekret(self):
+        response = self.client.get("/secret/1")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), {"detail": "Secret not found"})
+
+    def test_get_sekrets_by_space_id_with_no_sekrets(self):
+        response = self.client.get("/secret/space/1")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), {"detail": "Secret not found"})
+
+    def test_get_sekrets_popular_with_no_sekrets(self):
+        response = self.client.get("/secrets/popular")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [])
