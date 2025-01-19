0b5f0df656b47f04366bf921ce8f589866f454ed
Lytzeer <lukasportier@gmail.com>
2025-01-19 22:46:43 +0100

    Ajout de commentaires et retrait d'un doublon

--- tests/test_service_spaces.py
+++ tests/test_service_spaces.py
@@ -4,6 +4,9 @@ from src.spaces.service import delete_space, get_spaces_list, get_space
 
 class TestSpacesService(unittest.TestCase):
 
+    #############################
+    # GET - Get all spaces test cases
+
     @patch("src.spaces.service.SessionLocal")
     def test_get_spaces_list_empty(self, mock_session_local):
         mock_session = MagicMock()
@@ -11,6 +14,18 @@ class TestSpacesService(unittest.TestCase):
         mock_session_local.return_value.__enter__.return_value = mock_session
         result = get_spaces_list()
         self.assertEqual(result, [])
+
+    # TODO: Valeur de retour à modifier
+    @patch("src.spaces.service.SessionLocal")
+    def test_get_spaces_list(self, mock_session_local):
+        mock_session = MagicMock()
+        mock_session.query().all.return_value = [MagicMock()]
+        mock_session_local.return_value.__enter__.return_value = mock_session
+        result = get_spaces_list()
+        self.assertNotEqual(result, [])
+
+    #############################
+    # GET - Get a space by space_id test cases
 
     @patch("src.spaces.service.SessionLocal")
     def test_get_space_by_id_none(self, mock_session_local):
@@ -28,22 +43,8 @@ class TestSpacesService(unittest.TestCase):
         result = get_space(1)
         self.assertIsNotNone(result)
 
-    # TODO: Valeur de retour à modifier
-    @patch("src.spaces.service.SessionLocal")
-    def test_get_spaces_list(self, mock_session_local):
-        mock_session = MagicMock()
-        mock_session.query().all.return_value = [MagicMock()]
-        mock_session_local.return_value.__enter__.return_value = mock_session
-        result = get_spaces_list()
-        self.assertNotEqual(result, [])
-
-    @patch("src.spaces.service.SessionLocal")
-    def test_get_space_by_id_none(self, mock_session_local):
-        mock_session = MagicMock()
-        mock_session.query().filter().first.return_value = []
-        mock_session_local.return_value.__enter__.return_value = mock_session
-        result = get_space(1)
-        self.assertIsNone(result)
+    #############################
+    # DELETE - Delete a space test cases
 
     @patch("src.spaces.service.SessionLocal")
     def test_delete_space(self, mock_session_local):
