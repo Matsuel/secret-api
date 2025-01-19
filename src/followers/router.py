3c905414b91e30c110b1b027ecdd02fefc4b8287
Lytzeer <lukasportier@gmail.com>
2025-01-19 17:27:42 +0100

    Ajout des tests unitaire pour le followers router avec l'utilisaion des mock & Modification des codes HTTP pour les 2 routes GET du followers router

--- src/followers/router.py
+++ src/followers/router.py
@@ -7,11 +7,15 @@ followers_router = APIRouter()
 @followers_router.get("/user/{user_id}/follow", tags=["users"])
 def get_user_follows(user_id: int, offset: int = 0, limit: int = 100):
     follows = get_follows_in_db(user_id, offset, limit)
+    if not follows:
+        raise HTTPException(status_code=404, detail="No follows found")
     return follows
 
 @followers_router.get("/user/{user_id}/followers", tags=["users"])
 def get_user_followers(user_id: int, offset: int = 0, limit: int = 100):
     followers = get_followers_in_db(user_id, offset, limit)
+    if not followers:
+        raise HTTPException(status_code=404, detail="No followers found")
     return followers
 
 @followers_router.post("/user/{user_id}/follow/{followed_id}", tags=["users"])
