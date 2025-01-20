3a9942fd14464d2efa55280e3c3ae64c8d1c8c50
Lytzeer <lukasportier@gmail.com>
2025-01-20 22:42:44 +0100

    Ajout d'un nouveau test unitaire pour un fonction service pour les users

--- tests/test_service_users.py
+++ tests/test_service_users.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from src.users.service import hash_password, verify_password, create_user_in_db
+from src.users.service import get_user_by_id, hash_password, verify_password, create_user_in_db
 from src.models.user import User
 
 
@@ -39,3 +39,11 @@ class TestCreateUserInDB(unittest.TestCase):
         hashed_password = hash_password(password)
         self.assertTrue(verify_password(password, hashed_password))
         self.assertFalse(verify_password("wrong_password", hashed_password))
+
+    @patch("src.users.service.SessionLocal")
+    def test_get_user_by_id(self, mock_session_local):
+        mock_session = MagicMock()
+        mock_session.query().filter().first.return_value = MagicMock()
+        mock_session_local.return_value.__enter__.return_value = mock_session
+        result = get_user_by_id(1)
+        self.assertIsNotNone(result)
