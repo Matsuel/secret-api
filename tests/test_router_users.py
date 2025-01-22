0e28b36b5946e2725c6ee9ec681260cbe98fa6f7
Lytzeer <lukasportier@gmail.com>
2025-01-22 23:04:29 +0100

    Correction d'un test pour le users router pour passer par l'authentification

--- tests/test_router_users.py
+++ tests/test_router_users.py
@@ -67,18 +67,32 @@ class TestUsersRouter(unittest.TestCase):
     # PUT - Update a user by user_id test cases
 
     @patch("src.users.router.update_user_in_db")
-    def test_update_user_with_success(self, mock_update_user_in_db):
+    @patch("src.users.router.authenticate_user")
+    def test_update_user_with_success(self, mock_authenticate_user, mock_update_user_in_db):
+        # Simuler une authentification réussie
+        mock_authenticate_user.return_value = {"id": 1, "username": "test_authenticated_user"}
+        # Simuler un succès de la mise à jour
         mock_update_user_in_db.return_value = True
-        response = self.client.put("/user/1", json={"username": "test", "password": "test"})
+
+        # Envoyer la requête avec un token simulé
+        token = "Bearer mock_valid_token"
+        headers = {"Authorization": token}
+        payload = {"username": "test_username", "password": "test_password"}
+
+        response = self.client.put("/user/", headers=headers, json=payload)
+
+        # Vérifications
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"message": "User updated"})
 
     @patch("src.users.router.update_user_in_db")
-    def test_update_user_with_no_user(self, mock_update_user_in_db):
+    @patch("src.users.router.authenticate_user")
+    def test_update_user_with_no_user_and_no_auth(self, mock_update_user_in_db, mock_authenticate_user):
+        mock_authenticate_user.return_value = False
         mock_update_user_in_db.return_value = False
-        response = self.client.put("/user/1", json={"username": "test", "password": "test"})
-        self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.json(), {"detail": "User not found or username already exists"})
+        response = self.client.put("/user/", json={"username": "test", "password": "test"})
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"detail": "Unauthorized"})
 
     #############################
     # DELETE - Delete a user by user_id test cases
