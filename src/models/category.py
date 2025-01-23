660c71f9b95f0e9737a6279e6558feca881958ee
Matsuel <matsuel0308@gmail.com>
2025-01-23 18:58:28 +0100

    Add TODO

--- src/models/category.py
+++ src/models/category.py
@@ -3,11 +3,13 @@ from .database import Base
 from pydantic import BaseModel
 
 class Category(Base):
+    # TODO: user_id should be a foreign key to the users table
     __tablename__ = 'categories'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
     
 class CategoryModel(BaseModel):
+    # TODO: user_id should be a foreign key to the users table
     id: int
     name: str
     
