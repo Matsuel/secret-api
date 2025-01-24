8b69bb2e30ac0e0e2f16175413742180ad6ee79f
Matsuel <matsuel0308@gmail.com>
2025-01-24 21:46:57 +0100

    Add authentication with oauth2barear

--- src/users/router.py
+++ src/users/router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from src.auth import service as auth_service
 from fastapi import HTTPException
 from src.models.user import UserModelCreation
 from src.auth.service import authenticate_user
@@ -7,18 +8,14 @@ from src.users.service import get_users_list, create_user_in_db, delete_user_in_
 users_router = APIRouter()
 
 @users_router.get("/users", tags=["users"])
-def get_users(offset: int = 0, limit: int = 10, token: str = None):
-    if not authenticate_user(token):
-        raise HTTPException(status_code=401, detail="Unauthorized")
+async def get_users(offset: int = 0, limit: int = 10, current_user: dict = Depends(auth_service.get_current_user)):
     users = get_users_list(offset, limit)
     if not users:
         raise HTTPException(status_code=404, detail="No users found")
     return users
 
 @users_router.get("/user/{user_id}", tags=["users"])
-def get_user_infos(user_id: int, token: str = None):
-    if not authenticate_user(token):
-        raise HTTPException(status_code=401, detail="Unauthorized")
+def get_user_infos(user_id: int, current_user: dict = Depends(auth_service.get_current_user)):
     user = get_user_by_id(user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -32,21 +29,15 @@ def create_user(user: UserModelCreation):
     return {"message": "User created"}
 
 @users_router.put("/user/", tags=["users"], status_code=200)
-def update_user(user: UserModelCreation, token: str = None):
-    authenticated_user = authenticate_user(token)
-    if not authenticated_user:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-    result = update_user_in_db(authenticated_user['id'], user)
+def update_user(user: UserModelCreation, current_user: dict = Depends(auth_service.get_current_user)):
+    result = update_user_in_db(current_user['id'], user)
     if not result:
         raise HTTPException(status_code=404, detail="User not found or username already exists")
     return {"message": "User updated"}
 
 @users_router.delete("/user/", tags=["users"], status_code=200)
-def delete_user(token: str = None):
-    authenticated_user = authenticate_user(token)
-    if not authenticated_user:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-    result = delete_user_in_db(authenticated_user['id'])
+def delete_user(current_user: dict = Depends(auth_service.get_current_user)):
+    result = delete_user_in_db(current_user['id'])
     if not result:
         raise HTTPException(status_code=404, detail="User not found")
     return {"message": "User deleted"}
