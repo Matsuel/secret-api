6cd1266a474842c0ed62c941a3cb00d980d87b1c
Lytzeer <lukasportier@gmail.com>
2025-01-30 09:49:41 +0100

    Correction des tests du auth router

--- tests/test_router_auth.py
+++ tests/test_router_auth.py
@@ -1,26 +1,35 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 from src.auth.service import authenticate_user, create_access_token
-from src.users.service import authenticate_user, create_access_token
+from src.models.user import User
+
 class TestAuthService(unittest.TestCase):
-    @patch("src.auth.service.get_user_from_db")
-    def test_authenticate_user_success(self, mock_get_user):
-        mock_get_user.return_value = {
-            "id": 1,
-            "username": "test",
-            "hashed_password": "hashed_test"
-        }
-        user = authenticate_user("test", "test_password")
-        self.assertIsNotNone(user)
-        self.assertEqual(user["username"], "test")
-    
-    @patch("src.auth.service.get_user_from_db")
-    def test_authenticate_user_failure(self, mock_get_user):
-        mock_get_user.return_value = None
-        user = authenticate_user("invalid_user", "invalid_password")
-        self.assertIsNone(user)
+
+    @patch("src.auth.service.verify_token")
+    @patch("src.auth.service.get_user_by_id")
+    def test_authenticate_user_success(self, mock_get_user_by_id, mock_verify_token):
+        mock_verify_token.return_value = {"user_id": 1}
+        mock_user = Mock()
+        mock_user.username = "test"
+        mock_get_user_by_id.return_value = mock_user
+        token = "valid_token"
+        result = authenticate_user(token)
+        self.assertIsNotNone(result)
+        self.assertEqual(result.username, "test")
+
+    @patch("src.auth.service.verify_token")
+    def test_authenticate_user_failure(self, mock_verify_token):
+        mock_verify_token.return_value = None
+        invalid_token = "invalid_token"
+        result = authenticate_user(invalid_token)
+        self.assertFalse(result)
     
     def test_create_access_token(self):
         user = {"id": 1, "username": "test"}
+        user = User(
+            id = 1,
+            username = "test",
+            password = "test_password"
+        )
         token = create_access_token(user)
         self.assertIsNotNone(token)
